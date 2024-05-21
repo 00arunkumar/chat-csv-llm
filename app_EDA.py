b7416e4000b15e272601006f4b884e74a053df3e
Arun kumar <44183407+00arunkumar@users.noreply.github.com>
2024-05-21 10:46:23 +0530

    Update app_EDA.py

--- app_EDA.py
+++ app_EDA.py
@@ -3,13 +3,14 @@
 
 import streamlit as st
 import pandas as pd
+from openai import RateLimitError
 
 
 from langchain.llms import OpenAI
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 
 
-#OpenAIKey
+
 
 
 
@@ -63,6 +64,7 @@ if st.session_state.clicked[1]:
             st.write("**Data Overview**")
             st.write("The first rows of your dataset look like this:")
             st.write(df.head())
+            
             st.write("**Data Cleaning**")
             columns_df = pandas_agent.run("What are the meaning of the columns?")
             st.write(columns_df)
@@ -104,30 +106,31 @@ if st.session_state.clicked[1]:
 
 
         #Main
-
-        st.header('Exploratory data analysis')
-        st.subheader('General information about the dataset')
-
-        function_agent()
-
-        st.subheader('Variable of study')
-        user_question_variable = st.selectbox("what variables are you interested in ?", df.columns)
-        if user_question_variable is not None and user_question_variable !="":
-            function_question_variable()
-
+        try:
+            st.header('Exploratory data analysis')
+            st.subheader('General information about the dataset')
+            function_agent()
+            
+            st.subheader('Variable of study')
+            user_question_variable = st.selectbox("what variables are you interested in ?", df.columns)
+            if user_question_variable is not None and user_question_variable !="":
+                function_question_variable()
+                
             st.subheader('Further study')
+            
+            st.subheader('Suggestion Queries for your CSV file')
+            sugg_query = pandas_agent.run("Give me 5 best Analytical question which i can ask from this upload csv data ?.")
+            st.write(sugg_query)
+            
+            if user_question_variable:
+                user_question_dataframe = st.text_input( "Is there anything else you would like to know about your dataframe?")
+                if user_question_dataframe is not None and user_question_dataframe not in ("","no","No"):
+                    function_question_dataframe()
+                if user_question_dataframe in ("no", "No"):
+                    st.write("")
+        except RateLimitError :
+            st.error("You had used all your free tokens of your OpenAI API. Please enter a different OpenAI API Key.", icon="🚨")
+            st.image("https://static-00.iconduck.com/assets.00/robot-404-error-errors-illustration-2048x1704-clz3f1n7.png")
 
 
-        st.subheader('Suggestion Queries for your CSV file')
-        sugg_query = pandas_agent.run("Give me 5 best Analytical question which i can ask from this upload csv data ?.")
-        st.write(sugg_query)
-
-
-
-        if user_question_variable:
-            user_question_dataframe = st.text_input( "Is there anything else you would like to know about your dataframe?")
-            if user_question_dataframe is not None and user_question_dataframe not in ("","no","No"):
-                function_question_dataframe()
-            if user_question_dataframe in ("no", "No"):
-                st.write("")
 
